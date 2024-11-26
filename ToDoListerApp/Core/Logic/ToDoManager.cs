3ff5d4f21d3b370118af113a96f1532a4fc5d0ca
InspiredCoding <InspiredCoding@DESKTOP-G8UJ6KD>
2024-11-25 22:41:05 -0600

    Completed the project and published it

---
+++ ToDoListerApp/Core/Logic/ToDoManager.cs
@@ -0,0 +1,83 @@
+﻿using CodeToolbox.Framework;
+using ToDoListerApp.Core.Features;
+using ToDoListerApp.Core.Features.AddToDoTask;
+using ToDoListerApp.Core.Framework;
+using static CodeToolbox.Logic.StringHelper;
+
+namespace ToDoListerApp.Core.Logic
+{
+    public class ToDoManager(ILogger<ToDoManager> logger, LocalStorageContext localStorageContext)
+    {
+        private readonly List<TaskItem> Items = [];
+
+        private int GetTotalCount() => Items.Count;
+        private int GetCompletedCount() => Items.FindAll(x => x.Complete).Count();
+
+        private async Task UpdateTaskItems()
+        {
+            if(localStorageContext is not null)
+            {
+                await localStorageContext.SetItemsAsync<TaskItem>("Tasks", Items);
+            }
+        }
+
+        public string GetCompletionString()
+        {
+            var result = "(Currently No Tasks)";
+            if(GetTotalCount() > 0)
+            {
+                result = $"({this.GetCompletedCount()} of {this.GetTotalCount()}) {GetSingleOrPluralString("Task", this.GetTotalCount())} Complete";
+            }
+            return result;
+        }
+
+        public async Task<List<TaskItem>> GetItemsAsync()
+        {
+            if(GetTotalCount() == 0 && localStorageContext is not null)
+            {
+                var items = await localStorageContext.GetItemsAsync<TaskItem>("Tasks");
+                if(items is not null)
+                {
+                    Items.AddRange(items);
+                }
+            }
+            return Items;
+        }
+
+
+        public async Task AddTaskAsync(string taskName)
+        {
+            if (!string.IsNullOrWhiteSpace(taskName))
+            {
+                TaskItem item = new(Guid.CreateVersion7(), taskName);
+                Items.Add(item);
+                await UpdateTaskItems();
+                Log.TaskItemAdded(logger, item.Id.ToString());
+            }
+        }
+
+        public async Task ToggleComplete(Guid id)
+        {
+            var item = Items.Find(x => x.Id == id);
+            if(item is not null)
+            {
+                item.Complete = !item.Complete;
+                await UpdateTaskItems();
+                Log.TaskItemCompletionChanged(logger, item.Id.ToString(), item.Complete);
+            }
+            
+        }
+
+        public async Task DeleteTaskAsync(Guid id)
+        {
+            var item = Items.Find(x => x.Id == id);
+            if(item is not null)
+            {
+                Items.Remove(item);
+                await UpdateTaskItems();
+                Log.TaskItemDeleted(logger, item.Id.ToString());
+            }
+        }
+
+    }
+}
--- ToDoListerApp/Program.cs
+++ ToDoListerApp/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
+using CodeToolbox.Framework;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using ToDoListerApp;
+using ToDoListerApp.Core.Logic;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,7 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<LocalStorageContext>();
+builder.Services.AddScoped<ToDoManager>();
+
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddFluentUIComponents();
 builder.Services.AddDataGridEntityFrameworkAdapter();
+
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 await builder.Build().RunAsync();
